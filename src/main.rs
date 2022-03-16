05f355949c976b789b1146a4bca09ff2801fe06b
João Cardoso <jvcq13@gmail.com>
2022-03-15 22:31:38 -0300

    first commit

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+use colored::{self, Colorize};
+
+fn main() {
+
+    let mut rng = rand::thread_rng();
+    let secret_number = rng.gen_range(1..101);
+
+    println!("Guess the number!");
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+    
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);
+        
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("{}", "Too small!".red()),
+            Ordering::Greater => println!("{}", "Too Big!".red()),
+            Ordering::Equal => {
+                println!("{}", "You win!!".green());
+                break;
+            }
+        }
+    }
+
+}
+
